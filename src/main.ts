37d2229805978aca0128451ffacf73aa05811a8a
chriko3 <christoph.kohout03@gmail.com>
2026-05-27 18:32:21 +0200

    Implement exit game popup functionality and refine game exit styles

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import './styles/main.scss'
 document.addEventListener("contextmenu", e => e.preventDefault());
 
-function exitGame(){
+function exitGame() {
     window.location.href = "index.html";
 }
 (window as any).exitGame = exitGame;
+
+function togglePopUp() {
+    let exitPopUp = document.getElementById("exitPopUp");
+    if (exitPopUp!.style.display === "none") {
+        exitPopUp!.style.display = "flex";
+    } else {
+        exitPopUp!.style.display = "none";
+    }
+}
+(window as any).togglePopUp = togglePopUp;
